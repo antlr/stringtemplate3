ad16186cf418429cc9c163791e4b176a8b09bd8d
parrt <parrt@antlr.org>
2008-01-23 18:14:33 -0800

    set to 3.1
    [git-p4: depot-paths = "//depot/code/stringtemplate/java/main/": change = 4495]

--- src/org/antlr/stringtemplate/StringTemplate.java
+++ src/org/antlr/stringtemplate/StringTemplate.java
@@ -45,7 +45,7 @@ import antlr.collections.ASTEnumeration;
  *
  */
 public class StringTemplate {
-	public static final String VERSION = "3.1b1";
+	public static final String VERSION = "3.1"; // January 23, 2008
 
 	/** <@r()> */
 	public static final int REGION_IMPLICIT = 1;
