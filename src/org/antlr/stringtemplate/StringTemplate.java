129080f9c0cffeed0d7688a7356a6d62afde6326
parrt <parrt@antlr.org>
2008-08-12 10:48:07 -0800

    tweak version
    [git-p4: depot-paths = "//depot/code/stringtemplate/java/main/": change = 5204]

--- src/org/antlr/stringtemplate/StringTemplate.java
+++ src/org/antlr/stringtemplate/StringTemplate.java
@@ -45,7 +45,7 @@ import antlr.collections.ASTEnumeration;
  *
  */
 public class StringTemplate {
-	public static final String VERSION = "3.2"; // June 8, 2008
+	public static final String VERSION = "3.3a"; // August 11, 2008
 
 	/** <@r()> */
 	public static final int REGION_IMPLICIT = 1;
